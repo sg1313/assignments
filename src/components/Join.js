06f98a1ff27bfe7a8c37be33ea5746bad28d1a64
sg1313 <fpemvps2@gmail.com>
2022-10-09 13:19:26 +0900

    22.10.09 13:19

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css';
-import {Link} from 'react-router-dom';
+import {Link, Navigate, useNavigate} from 'react-router-dom';
 
 const Join = () => {
+
+    const [userid, setUserid] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [birth, setBirth] = useState('');
+
+    const onUseridHandler = (e) => {
+        setUserid(e.currentTarget.value);
+        console.log('userid찍히나',setUserid);
+        localStorage.setItem('userid', e.currentTarget.value);
+        
+    };
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value);
+        console.log('비번찍히나',setPassword);
+        localStorage.setItem('password', e.currentTarget.value);
+        
+    };
+    const onUsernameHandler = (e) => {
+        setUsername(e.currentTarget.value);
+        console.log('이름찍히나',setUsername);
+        localStorage.setItem('username', e.currentTarget.value);
+
+    };
+    const onBirthHandler = (e) => {
+        setBirth(e.currentTarget.value);
+        console.log('생일찎히나',setBirth);
+        localStorage.setItem('birth', e.currentTarget.value);
+    };
+
     return (
         <div>
                 <div className='login'>
-                아이디 : <input type='text' key={userId}/> <br/>
-                비밀번호 : <input type='password' key={userPw}/> <br/>
-                이름 : <input type='text' key={userName}/> <br/>
-                생년월일 : <input type='date' key={userBirth}/> <br/>
-                <button>작성완료</button> <button>작성취소</button>
+                    <form>
+                아이디 : <input type='text' name='userid' key={'userid'} onChange={onUseridHandler}/> <br/>
+                비밀번호 : <input type='password' name='password' onChange={onPasswordHandler}/> <br/>
+                이름 : <input type='text' name='username' onChange={onUsernameHandler}/> <br/>
+                생년월일 : <input type='date' name='birth' onChange={onBirthHandler}/> <br/>
+                <Link to='/'><button type='submit'>작성완료</button></Link> <button>작성취소</button>
+           </form>
             </div>
                 <Link to='/'>메인</Link>
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './List.css';
 import { Link } from 'react-router-dom';
 
+
+// https://joonganglib.tistory.com/13
+// https://www.saichoiblog.com/react-localstorage/
+// https://intrepidgeeks.com/tutorial/implement-react-registration-and-login-costs
+
+
 const Login = () => {
 
-    // const [id, setId] = useState(''); 
-    // const [pw, setPw] = useState('');
+    const [userid, setUserid] = useState(''); 
+    const [password, setPassword] = useState('');
 
+    const HandleIdChange = (e) => {
+        localStorage.getItem(userid);
+        console.log('아이디',userid);
+    }
+    const HandlePwChange = (e) => {
+        localStorage.getItem(password);
+        console.log('비밀번호', password);
+    } 
 
+    const onClickHandler = (e) => {
+        e.preventDefault();
+    }
     return (
         <div>  {/* p. 192 */}
+        
             <div className='login'>
-                아이디 : <input type='text'/> <br/><br/>
-                비밀번호 : <input type='password'/> <br/><br/>
+                아이디 : <input type='text' name='userid' onChange={HandleIdChange}/> <br/><br/>
+                비밀번호 : <input type='password' name='password' onChange={HandlePwChange}/> <br/><br/>
                 <Link to='/join'><button>회원가입</button> </Link>
-                <button>로그인</button>
+                <button onChange={onClickHandler}>로그인</button>
             </div>
+            
         </div>
     )
 }
